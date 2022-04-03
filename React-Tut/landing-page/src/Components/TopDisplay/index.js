7a3acbafeb6243afec234dd8919aa4fd344dbf03
Vishnu <vishnusingh1057@gmail.com>
2022-04-03 11:25:55 +0530

    Library Installed
    Some Libraries were installed and react version was downgraded as i was unable to download MUI otherwise

---
+++ React-Tut/landing-page/src/Components/TopDisplay/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+const TopDisplay  = () =>{
+    return(
+    <>
+        <div className='topBar'>
+            TopBar
+        </div>
+    </>
+    );
+}
+export default TopDisplay
