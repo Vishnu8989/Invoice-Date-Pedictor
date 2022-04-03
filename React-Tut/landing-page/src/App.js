9f0a1964e3733e0fe8b15a850ddafc28b6d3da81
Vishnu <vishnusingh1057@gmail.com>
2022-04-03 13:00:51 +0530

    Temp

--- React-Tut/landing-page/src/App.js
+++ React-Tut/landing-page/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./Components/TopDisplay";
+import Buttons from "./Components/BottonBar";
 
 function App() {
   return (
     <div>
       <Navbar/>
+        <Buttons/>
     </div>
   );
 }
---
+++ React-Tut/landing-page/src/Components/BottonBar/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const Buttons = ()=>{
+    return(
+        <>
+            <div className="container">
+
+            </div>
+        </>
+    );
+}
+export default Buttons
